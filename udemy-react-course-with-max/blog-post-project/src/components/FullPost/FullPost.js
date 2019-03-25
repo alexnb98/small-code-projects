28d8ee6e74f5e07726ec42e5e508a977e6522966
Alexander Becker <alexandernino98@gmail.com>
2019-03-25 14:55:04 +0100

    learned how to combine axios with react

--- udemy-react-course-with-max/blog-post-project/src/components/FullPost/FullPost.js
+++ udemy-react-course-with-max/blog-post-project/src/components/FullPost/FullPost.js
@@ -1,32 +1,55 @@
 import React, { Component } from 'react';
-
 import './FullPost.css';
-import { Axios } from 'axios';
+import axios from 'axios';
 
 class FullPost extends Component {
 	state = {
-		loadedPost: null
+		loadedPost: null,
+		error: false
 	};
 
 	componentDidUpdate() {
-		if (this.props.id) {
-			Axios.get('https://jsonplaceholder.typicode.com/posts' + this.props.id).then((response) => {
-				console.log(response);
-			});
+		if (this.props.id && !this.state.error) {
+			if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+				axios
+					.get('/posts/' + this.props.id)
+					.then((response) => {
+						this.setState({ loadedPost: response.data });
+					})
+					.catch((error) => {
+						this.setState({ error: true });
+						console.log('error', error);
+					});
+			}
 		}
 	}
 
+	deletePostHandler = () => {
+		axios.delete('/posts/' + this.props.id).then((response) => console.log('response', response));
+	};
+
 	render() {
-		let post = <p className={{ textAlign: 'center' }}>Please select a Post!</p>;
-		post = (
-			<div className="FullPost">
-				<h1>Title</h1>
-				<p>Content</p>
-				<div className="Edit">
-					<button className="Delete">Delete</button>
+		let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+
+		if (this.props.id) {
+			post = <p style={{ textAlign: 'center' }}>Loading...</p>;
+		}
+		if (this.state.loadedPost) {
+			post = (
+				<div className="FullPost">
+					<h1>{this.state.loadedPost.title}</h1>
+					<p>{this.state.loadedPost.body}</p>
+					<div className="Edit">
+						<button onClick={this.deletePostHandler} className="Delete">
+							Delete
+						</button>
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
+		if (this.state.error) {
+			post = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+		}
 		return post;
 	}
 }
--- udemy-react-course-with-max/blog-post-project/src/containers/Blog/Blog.js
+++ udemy-react-course-with-max/blog-post-project/src/containers/Blog/Blog.js
@@ -4,16 +4,16 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import Axios from 'axios';
+import axios from '../../axios';
 
 class Blog extends Component {
 	state = {
 		posts: [],
-		selectedPost: null
+		selectedPostId: null
 	};
 
 	componentDidMount() {
-		Axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+		axios.get('/posts').then((response) => {
 			const posts = response.data.slice(0, 4);
 			const updatedPosts = posts.map((post) => {
 				return { ...post, author: 'Alexander' };
@@ -23,7 +23,7 @@ class Blog extends Component {
 	}
 
 	selectedPostHandler = (id) => {
-		this.setState({ selectedPost: id });
+		this.setState({ selectedPostId: id });
 	};
 
 	render() {
@@ -41,7 +41,7 @@ class Blog extends Component {
 			<div>
 				<section className="Posts">{posts}</section>
 				<section>
-					<FullPost id={this.state.selectedPost} />
+					<FullPost id={this.state.selectedPostId} />
 				</section>
 				<section>
 					<NewPost />
