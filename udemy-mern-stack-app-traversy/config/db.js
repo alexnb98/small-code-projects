b1fcbfe73022244a65555bbcef3ce78ffbf37994
Alexander Becker <alexandernino98@gmail.com>
2019-05-04 17:11:07 +0200

    MERM created server and connected with mongoDB

---
+++ udemy-mern-stack-app-traversy/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const mongoURI = config.get('mongoURI');
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(mongoURI, {
+			useNewUrlParser: true
+		});
+		console.log('MongoDB Connected...');
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
