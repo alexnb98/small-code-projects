438f24adea7a1327267073c2c7a3f5b8f1fa282a
Alexander Becker <alexandernino98@gmail.com>
2019-05-05 20:02:39 +0200

    MERN creating Restful API

--- udemy-mern-stack-app-traversy/config/db.js
+++ udemy-mern-stack-app-traversy/config/db.js
@@ -5,7 +5,9 @@ const mongoURI = config.get('mongoURI');
 const connectDB = async () => {
 	try {
 		await mongoose.connect(mongoURI, {
-			useNewUrlParser: true
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false
 		});
 		console.log('MongoDB Connected...');
 	} catch (error) {
