b9b69dd9b638001928803ecbd911687610c24f8a
Alexander <alexandernino98@gmail.com>
2019-05-25 16:25:20 +0200

    Finished Interactive Interview

--- interactive-interview/src/App.js
+++ interactive-interview/src/App.js
@@ -1,77 +1,77 @@
 import React, {Component} from "react";
-import {Typography, Button, Grid, Paper} from "@material-ui/core";
+import {Typography, Container, Grid, Paper} from "@material-ui/core";
 import {questions} from "./Data/questions";
 
 class App extends Component {
     state = {
         questions: [],
         answers: [],
-        selectedQ: []
+        randomIndexes: []
     };
 
     componentDidMount() {
-        this.setState({questions});
+        this.setState({questions}, () => this.generateRandomIndexes());
     }
 
-    getRandomQuestions = () => {
-        if (this.state.questions.length > 0) {
-            for (let i = 0; i < 3; i++) {
-                this.setState(state => {
-                    const newQuestions = state.questions;
-                    const newSelectedQ = state.selectedQ;
-                    console.log("newQuestions", newQuestions);
-                    const ranNum = Math.floor(Math.random() * newQuestions.length);
-                    const selectedQ = newQuestions.splice(ranNum, 1);
-                    newSelectedQ.push(selectedQ[0]);
-                    return {questions: newQuestions, selectedQ: newSelectedQ};
-                });
-            }
-        }
+    selectQuestion = id => {
+        const questions = [...this.state.questions];
+        const index = questions.findIndex(el => el.id === id);
+        const question = questions.splice(index, 1);
+        const answers = [...this.state.answers, ...question];
+        this.setState({questions, answers}, () => this.generateRandomIndexes());
     };
 
-    selectQuestion = id => {
-        this.setState(state => {
-            const newSelectQ = state.selectedQ;
-            const index = newSelectQ.findIndex(el => el.id === id);
-            const question = newSelectQ.splice(index, 1);
-            return {
-                questions: [...state.questions, ...newSelectQ],
-                answers: [...state.answers, question[0]],
-                selectedQ: []
-            };
-        });
-        this.getRandomQuestions();
+    generateRandomIndexes = () => {
+        const max = this.state.questions.length;
+        const quantity = max > 3 ? 3 : max;
+        const randomIndexes = [];
+        while (randomIndexes.length < quantity) {
+            const ranNum = Math.floor(Math.random() * max);
+            if (!randomIndexes.includes(ranNum)) {
+                randomIndexes.push(ranNum);
+            }
+        }
+        this.setState({randomIndexes});
     };
 
     render() {
-        let questions = <div>No questions</div>;
-        if (this.state.selectedQ.length > 0) {
-            questions = this.state.selectedQ.map(q => {
-                return (
-                    <Paper onClick={() => this.selectQuestion(q.id)} key={q.id} style={{padding: "1rem", marginBottom: "1rem"}}>
-                        <div>{q.question}</div>
+        const {questions, randomIndexes, answers} = this.state;
+        let renderQuestions = <div>No more Questions</div>;
+        if (randomIndexes.length) {
+            renderQuestions = randomIndexes.map(i => {
+                return questions[i] ? (
+                    <Paper key={questions[i].id} onClick={() => this.selectQuestion(questions[i].id)} style={{padding: "1rem", marginBottom: "1rem"}}>
+                        {questions[i].question}
                     </Paper>
-                );
+                ) : null;
             });
         }
-        let answer = <div>Select a question</div>;
-        if (this.state.answers.length > 0) {
-            answer = <div> {this.state.answers[this.state.answers.length - 1].answer}</div>;
+
+        let renderAnswer = <div>Select Question</div>;
+        if (answers.length) {
+            renderAnswer = answers[answers.length - 1] ? <Paper style={{padding: "1rem"}}>{answers[answers.length - 1].answer}</Paper> : "error";
         }
         return (
             <div className="App">
                 <Typography variant="h1" gutterBottom align="center">
                     Interactive Interview
                 </Typography>
-                <Grid container justify="center">
-                    <Grid item xs={8}>
-                        {questions}
-                        {answer}
-                        <Button variant="contained" onClick={this.getRandomQuestions}>
-                            Start
-                        </Button>
+                <Container maxWidth="md">
+                    <Grid container justify="center" spacing={8}>
+                        <Grid item xs={12} md={6}>
+                            <Typography variant="h2" align="center" gutterBottom>
+                                Questions
+                            </Typography>
+                            {renderQuestions}
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Typography variant="h2" align="center" gutterBottom>
+                                Answers
+                            </Typography>
+                            {renderAnswer}
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Container>
             </div>
         );
     }
--- interactive-interview/src/Data/questions.js
+++ interactive-interview/src/Data/questions.js
@@ -1,23 +1,25 @@
 export const questions = [
     {
         id: 1,
-        question: "Question 1",
-        answer: "Answer 1"
+        question: "Can you tell me a little bit about yourself?",
+        answer:
+            "I'm currently studying economics in the university of Heidelberg. I was born in Venezuela, but also grew up in Germany. I'm a self thought web developer and love coding."
     },
     {
         id: 2,
-        question: "Question 2",
-        answer: "Answer 2"
+        question: "How old are you?",
+        answer: "I'm 21 years old. "
     },
     {
         id: 3,
-        question: "Question 3",
-        answer: "Answer 3"
+        question: "What technologies have you learned in web development?",
+        answer:
+            "I started with HTML, CSS and basic JavaScript. I worked almost 1 year in chocoBRAIN, there I build themes and components, I learned Bootstrap.css, Jekyll, Liquid and how to work with teams using JIRA and git. To build my own apps I learned the MERN Stack (MongoDB, Express, React and Node.js). For my personal website I used Gatsby.js  "
     },
     {
         id: 4,
-        question: "Question 4",
-        answer: "Answer 4"
+        question: "Dogs or Cats?",
+        answer: "Dogs...definitely dogs...🐶"
     },
     {
         id: 5,
